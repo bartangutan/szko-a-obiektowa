0b01e409e5d62666408fa4f3549fba097a165b25
bartangutan <19bartosz11@gmail.com>
2025-10-27 15:20:18 +0100

    Klasa

---
+++ src/Klasa.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Klasa {
+    private Wychowawca wychowawca;
+    private ArrayList<Uczen> uczniowie;
+    private String nazwaKlasy;
+    //myślimy coi ta klasa powinna wiedzieć
+
+
+    public Klasa(Wychowawca wychowawca, ArrayList<Uczen> uczniowie, String nazwaKlasy) {
+        this.wychowawca = wychowawca;
+        this.uczniowie = uczniowie;
+        this.nazwaKlasy = nazwaKlasy;
+    }
+
+    public Klasa(String nazwaKlasy) {
+        this.nazwaKlasy = nazwaKlasy;
+        uczniowie = new ArrayList<>();
+    }
+}
